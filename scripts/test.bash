b59aae5f015aea70a2cd4245c67b498119f357d2
YazawaKenichi <tonfarken83825@gmail.com>
2022-10-30 02:03:33 +0900

    不正入力に対する挙動のテスト

--- scripts/test.bash
+++ scripts/test.bash
@@ -12,7 +12,16 @@ res=0
 out=$(seq 5 | ./plus)
 # ${LINENO} はその時の行番号が入る
 # || の左側で異常コードが来たら右側を実行
-[ "${out}" = 14.0 ] || ng ${LINENO}
+[ "${out}" = 15.0 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$1" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 # && の左側が成功したら右側を実行
 [ "$res" = 0 ] && echo OK
