57538e7860acc3674f37430b64fb5203908656c7
YazawaKenichi <tonfarken83825@gmail.com>
2022-11-03 12:20:53 +0900

    test.bash シバンの修正と Python ファイルを実行するようにした

--- test.bash
+++ test.bash
@@ -1,6 +1,11 @@
-#!/usr/bash
+#!/bin/bash
+# ↑　ここミスってても GitHub Actions は正常に実行できる
+# GitHub Actions は /bin/bash を使っていないから
 
 pwd
+# GitHub Actions 上では
+# /home/runner/work/REPOSITORY/REPOSITORY
+# が出力される。
 
-./scripts/plus_stdin.py < nums
+./scripts/plus_stdin.py < ./scripts/nums
 
